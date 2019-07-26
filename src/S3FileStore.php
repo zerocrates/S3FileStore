8c2a26f6ae0eb8d6aefb372fcb4fba7b8f3f4231
John Flatness <john@zerocrates.org>
2019-07-25 22:05:23 -0400

    Allow credential caching, fix content-type issue

--- src/S3FileStore.php
+++ src/S3FileStore.php
@@ -1,7 +1,10 @@
 <?php
 namespace S3FileStore;
 
+use Aws\DoctrineCacheAdapter;
 use Aws\S3\S3Client;
+use Doctrine\Common\Cache\ApcuCache;
+use finfo;
 use Omeka\File\Store\StoreInterface;
 
 class S3FileStore implements StoreInterface
@@ -16,19 +19,30 @@ class S3FileStore implements StoreInterface
      */
     private $bucket;
 
+    /**
+     * @var finfo
+     */
+    private $finfo;
+
     public function __construct($options)
     {
         $clientParams = $options['client_params'];
+        if (!isset($clientParams['credentials']) && extension_loaded('apcu')) {
+            $clientParams['credentials'] = new DoctrineCacheAdapter(new ApcuCache);
+        }
         $this->client = new S3Client($clientParams);
         $this->bucket = $options['bucket'];
+        $this->finfo = new finfo(FILEINFO_MIME_TYPE);
     }
 
     public function put($source, $storagePath)
     {
+        $mediaType = $this->finfo->file($source);
         $this->client->putObject([
             'Bucket' => $this->bucket,
             'Key' => $storagePath,
             'SourceFile' => $source,
+            'ContentType' => $mediaType,
         ]);
     }
 
@@ -44,5 +58,4 @@ class S3FileStore implements StoreInterface
     {
         return $this->client->getObjectUrl($this->bucket, $storagePath);
     }
-
 }
